6cadc43e6244abc3eeed5d737ad832153ef9dac8
rosmontisu <rosmontisu@gmail.com>
2023-08-03 17:48:50 +0900

    2023/08/03 commit in workplace
    루틴 구축 추가

--- BaaMain/routinesMainList.py
+++ BaaMain/routinesMainList.py
@@ -1,4 +1,5 @@
 from asyncio.windows_events import NULL
+from tkinter import Menubutton
 from ppadb.client import Client as AdbClient
 import os # 터미널 접근용
 import adbCtrlFuns as adbFun    # adb 관련 함수 모음
@@ -9,17 +10,18 @@ import pyautogui
 from mainSystem import pathFindImage, pathScreenImage, deviceInfo
 
 # 메뉴화면
-menuList = ["Cafe", "Schedule", "Student",        # 카페, 스케쥴, 학생
+menuList = ["Cafe", "Schedule", "Student",      # 카페, 스케쥴, 학생
             "Formation", "Club", "Crafting",    # 편성, 서클, 제조
             "Shop", "Recruit", "Campaign",      # 상점, 모집, 업무
-            "Tasks", "Ap", "Mail"               # 미션, ap, 우편함
-            ]   
+            "Tasks", "Ap", "Mail"]              # 미션, ap, 우편함
+              
 
 
 # 범용UI
 universalList = ["Back", "Home",                # 뒤로가기, 홈
-                 "ExitUseX", "ExitUseNotX",     # x, 빈공간 클릭
-                 "ZoomOut", "ZoomIn"]           # 줌아웃, 줌인
+                 "ExitUseX", "ExitUseNotX", "okButton",     # x, 빈공간 클릭
+                 "ZoomOut", "ZoomIn",           # 줌아웃, 줌인
+                 "WheelDown", "WheelUp"]        # 휠다운, 휠업
    
 # 카페 
 cafeList = ["Gifts", "Invitation", "CafeEarnings", ] # 선물, 초대, 수익
@@ -75,11 +77,27 @@ def formationRoutine():
     NULL
 # 5. 서클
 def clanRoutine():
+    menuButton("ExitUseX")
+    menuButton("Home")
 # 6. 제조
 def craftingRoutine():
     NULL
 # 7. 상점
 def shopRoutine():
+    menuButton("WheelDown") * 3
+
+#- 상단 파랑, 노랑, 보라 선택
+#- 일정 거리 위에서 아래로 드래그
+#- 중단 파랑, 노랑, 보라, 하양, 우상단 하양 선택
+#- 우하단 선택 구매 클릭
+- 확인 클릭
+    menuButton("ExitUseNotX")
+- 좌측 전술 대회 선택
+- 에너지 드링크 30, 60 선택 
+- 우하단 선택 구매 클릭
+    menuButton("okButton")
+    menuButton("ExitUseNotX")
+- 우상단 홈버튼
 
 
 # 8. 업무
@@ -104,27 +122,4 @@ def campaignRoutine():
 
 
 
-
-
-
-    
-
-
-
-
-
-
-
-
-
-def Lesson():
-def Student():
-def Formation():
-def Crafting():
-def Shop():
-def Recruit():
-def Campaign():
-def Tasks():
-def Ap():
-def Mail():
     
