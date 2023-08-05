b08705181e5e8dfc400dedf1a22b30275ff15ed0
rosmontisu <rosmontisu@gmail.com>
2023-08-05 17:27:28 +0900

    new file:   _testDir/tPPOCR.py 	new file:   resource/BlueArchive-UI-Img/image-removebg-preview (1).png 	new file:   resource/BlueArchive-UI-Img/image-removebg-preview (2).png 	new file:   resource/BlueArchive-UI-Img/image-removebg-preview (3).png 	new file:   resource/BlueArchive-UI-Img/image-removebg-preview (4).png 	new file:   resource/BlueArchive-UI-Img/image-removebg-preview (5).png 	new file:   resource/BlueArchive-UI-Img/image-removebg-preview (6).png 	new file:   resource/BlueArchive-UI-Img/image-removebg-preview (7).png 	new file:   resource/BlueArchive-UI-Img/image-removebg-preview.png 	new file:   resource/BlueArchive-UI-Img/pathImgUser.json 	deleted:    resource/BlueArchive-UI-Img/searchImage1.png 	deleted:    resource/BlueArchive-UI-Img/searchImage2.png 	modified:   src/ProjectCore/__pycache__/managerAdb.cpython-311.pyc 	modified:   src/ProjectCore/__pycache__/managerJson.cpython-311.pyc 	modified:   src/ProjectCore/managerJson.py 	modified:   src/ProjectCore/pathImgUser.json 	modified:   src/ProjectCore/routinesMain.py 	renamed:    src/ProjectWpfGui/.vs/WpfGui/FileContentIndex/0e70b665-c969-450e-8b09-1d591d91895d.vsidx -> src/ProjectWpfGui/.vs/WpfGui/FileContentIndex/0c71ae0a-52a0-4940-8fff-35329b226da2.vsidx 	modified:   src/ProjectWpfGui/.vs/WpfGui/v17/.suo 	new file:   src/ProjectWpfGui/WpfApp1/.vs/ProjectSettings.json 	new file:   src/ProjectWpfGui/WpfApp1/.vs/VSWorkspaceState.json 	deleted:    src/ProjectWpfGui/WpfApp1/.vs/WpfApp1/FileContentIndex/7f486de6-952f-4632-9efd-46e9de7c87db.vsidx 	new file:   src/ProjectWpfGui/WpfApp1/.vs/WpfApp1/FileContentIndex/de736c5d-15da-4414-8058-1ac14f9f3867.vsidx 	new file:   src/ProjectWpfGui/WpfApp1/.vs/WpfApp1/v17/.wsuo 	new file:   src/ProjectWpfGui/WpfApp1/.vs/slnx.sqlite 	modified:   src/ProjectWpfGui/WpfApp1/MainWindow.xaml 	modified:   src/ProjectWpfGui/WpfApp1/bin/Debug/WpfApp1.exe 	modified:   src/ProjectWpfGui/WpfApp1/bin/Debug/WpfApp1.pdb 	modified:   src/ProjectWpfGui/WpfApp1/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache 	modified:   src/ProjectWpfGui/WpfApp1/obj/Debug/MainWindow.g.cs 	modified:   src/ProjectWpfGui/WpfApp1/obj/Debug/MainWindow.g.i.cs 	modified:   src/ProjectWpfGui/WpfApp1/obj/Debug/WpfApp1.csproj.CoreCompileInputs.cache 	modified:   src/ProjectWpfGui/WpfApp1/obj/Debug/WpfApp1.csproj.FileListAbsolute.txt 	modified:   src/ProjectWpfGui/WpfApp1/obj/Debug/WpfApp1.csproj.GenerateResource.cache 	modified:   src/ProjectWpfGui/WpfApp1/obj/Debug/WpfApp1.exe 	modified:   src/ProjectWpfGui/WpfApp1/obj/Debug/WpfApp1.pdb 	modified:   src/ProjectWpfGui/WpfApp1/obj/Debug/WpfApp1_MarkupCompile.cache 	modified:   src/ProjectWpfGui/WpfApp1/obj/Debug/WpfApp1_MarkupCompile.lref 	modified:   src/ProjectWpfGui/WpfApp1/obj/Release/MainWindow.g.cs 	modified:   src/ProjectWpfGui/WpfApp1/obj/Release/WpfApp1_MarkupCompile.cache 	modified:   src/ProjectWpfGui/WpfApp1/obj/Release/WpfApp1_MarkupCompile.lref 	new file:   temp/nowScreen.png 	new file:   wheelhouse/PyMuPDF-1.22.5-cp311-cp311-win_amd64.whl

--- src/ProjectCore/managerJson.py
+++ src/ProjectCore/managerJson.py
@@ -1,16 +1,16 @@
 import json
 
 
-def imgPathsCreate(editMyPath):
-    global imgPath
-    imgPath = "pathImgUser.json"
+def imgPathsCreate(pathImgResource):
+
+    imgPath = "./src/ProjectCore/pathImgUser.json"
     with open(imgPath, "r") as f:
         data = json.load(f)
 
-    if data["system"]["firstOpen"] != editMyPath:
+    if data["system"]["firstOpen"] != pathImgResource:
         for key, value in data.items():
             for subkey, subvalue in value.items():
-                data[key][subkey] = f"{editMyPath}{subvalue}"
+                data[key][subkey] = f"{pathImgResource}{subvalue}"
     
         with open(imgPath, "w") as f:
             json.dump(data, f, indent=4)
--- src/ProjectCore/routinesMain.py
+++ src/ProjectCore/routinesMain.py
@@ -9,39 +9,6 @@ import pyautogui
 # main에서 path와 device 가져와둠
 from mainSystem import pathImg, pathScreen, deviceInfo
 
-menuImgPath = {
-    "Cafe": pathImg + "cafe.png", # 카페
-    "Invitation": pathImg + "invitation.png", # 초대
-    "ZoomOut": pathImg + "zoom_out.png", # 줌 아웃
-    "Gifts": pathImg + "gifts.png", # 선물
-    "CafeEarnings": pathImg + "cafe_earnings.png", # 카페 수익
-    "Receive": pathImg + "receive.png", # 수령
-    "Home": pathImg + "home.png", # 홈
-    "Schedule": pathImg + "schedule.png", # 스케쥴
-    "RightArrow": pathImg + "right_arrow.png", # 오른쪽 화살표
-}
-universalImgPath = {
-    "Back": pathImg + "back.png", # 뒤로 가기
-    "Home": pathImg + "home.png", # 홈
-    "ExitUseX": pathImg + "exit_use_x.png", # x
-    "ExitUseNotX": pathImg + "exit_use_not_x.png", # 빈 공간 클릭
-    "okButton": pathImg + "ok_button.png", # 확인 버튼
-    "ZoomOut": pathImg + "zoom_out.png", # 줌 아웃
-    "ZoomIn": pathImg + "zoom_in.png", # 줌 인
-    "WheelDown": pathImg + "wheel_down.png", # 휠 다운
-    "WheelUp": pathImg + "wheel_up.png", # 휠 업
-}
-cafeImgPath = {
-    "Gifts": pathImg + "gifts.png", # 선물
-    "Invitation": pathImg + "invitation.png", # 초대
-    "CafeEarnings": pathImg + "cafe_earnings.png", # 카페 수익
-}
-scheduleImgPath = {
-    "LeftArrow": pathImg + "left_arrow.png", # 왼쪽 화살표
-    "RightArrow": pathImg + "right_arrow.png", # 오른쪽 화살표
-    "Facility": pathImg + "facility.png", # 시설
-}
-
 # 메뉴 이름 찾아서 클릭
 def menuButton(imgName):
     imgName = pathImg + imgName + ".png" 
@@ -97,25 +64,14 @@ def craftingRoutine():
 def shopRoutine():
     menuButton("WheelDown") * 3
 
-#- 상단 파랑, 노랑, 보라 선택
-#- 일정 거리 위에서 아래로 드래그
-#- 중단 파랑, 노랑, 보라, 하양, 우상단 하양 선택
-#- 우하단 선택 구매 클릭
-- 확인 클릭
-    menuButton("ExitUseNotX")
-- 좌측 전술 대회 선택
-- 에너지 드링크 30, 60 선택 
-- 우하단 선택 구매 클릭
-    menuButton("okButton")
-    menuButton("ExitUseNotX")
-- 우상단 홈버튼
-
 
 # 8. 업무
 def recruitRoutine():
+    NULL
 
 # 9. 임무
 def campaignRoutine():
+    NULL
 
 
 
