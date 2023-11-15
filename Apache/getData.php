938d0971c00dc54fbac0ba5ad44c7a603aab65cc
hepari <107000683+rosmontisu@users.noreply.github.com>
2023-11-16 00:05:43 +0900

    Delete Apache

--- Apache/getData.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-// 클라이언트로부터 전달받은 데이터를 json에 담아서 출력
-$send["input1"]   =  $_POST['input1'];
-$send["input2"]   =  $_POST['input2'];
-$send["file"] =  $_FILES['fileInput'];
-echo json_encode($send);
-?>
